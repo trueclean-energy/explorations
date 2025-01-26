ef0ac7e6ffa15650287c23beefc2064f844cb632
Rahul Agarwal <111393043+rahulagarwal13@users.noreply.github.com>
2025-01-26 15:59:49 -0800

    Move files from src to app directory

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import EventTreeDiagram from '../components/EventTreeDiagram'
-
-export default function Home() {
-  return (
-    <main className="p-4">
-      <EventTreeDiagram />
-    </main>
-  )
-}
